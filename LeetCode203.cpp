13ac70c82bbce2b3307f2f5353367b2f3c5cfac1
ZhangHaosong123 <58296583+ZhangHaosong123@users.noreply.github.com>
2020-02-27 14:59:50 +0800

    LeetCode203

---
+++ LeetCode203.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode *p = head;
+    while(p && p->val == val) {
+        p = p->next;
+    }
+    head = p;
+    while (p && p->next) {
+        if (p->next->val == val) {
+            p->next = p->next->next;
+        }
+        else p = p->next;
+    }
+    return head;
+}
